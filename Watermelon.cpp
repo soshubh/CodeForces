33faeca36189b0c7cea02f3ed21590645b2a68ce
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-21 18:48:52 +0530

    Create Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int w,f=0;
+ cin>>w;
+  for(int i=2;i<w;i+=2)
+   for(int j=2;j<w;j+=2)
+   {
+       if(i+j==w){
+         f=1;
+         break;
+       }
+   }
+  if(f==1)
+   cout<<"YES";
+  else
+    cout<<"NO"; 
+ return 0; 
+}
