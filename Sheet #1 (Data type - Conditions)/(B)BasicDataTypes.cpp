21aeb4de6b3c87c61a2e8bc7c19f4c2b70950601
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-22 21:21:53 +0530

    Create (B)BasicDataTypes.cpp

---
+++ Sheet #1 (Data type - Conditions)/(B)BasicDataTypes.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  long long int lln;
+  char ch;
+  float f;
+  double d;
+  cin>>n>>lln>>ch>>f>>d;
+  cout<<n<<endl<<lln<<endl<<ch<<endl<<f<<endl<<d;
+ return 0; 
+}
