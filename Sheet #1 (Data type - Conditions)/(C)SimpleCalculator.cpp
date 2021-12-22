ef0365a6240af22a980644c5d39982639dc537ad
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-22 21:22:33 +0530

    Create (C)SimpleCalculator.cpp

---
+++ Sheet #1 (Data type - Conditions)/(C)SimpleCalculator.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long int a,b,r;
+  cin>>a>>b;
+  cout<<a<<" + "<<b<<" = "<<a+b<<endl;
+  cout<<a<<" * "<<b<<" = "<<a*b<<endl;  
+  cout<<a<<" - "<<b<<" = "<<a-b; 
+ return 0; 
+}
