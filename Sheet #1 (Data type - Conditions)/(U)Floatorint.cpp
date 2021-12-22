25483b5b38f2d32eecf2d24f4b93d7caeadadbd2
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-22 21:42:28 +0530

    Create (U)Floatorint.cpp

---
+++ Sheet #1 (Data type - Conditions)/(U)Floatorint.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ double num;
+  cin>>num;
+  int l=num;
+ 
+  if(num==l)
+  {
+      cout<<"int "<<l;
+  }
+  else
+    cout<<"float "<<l<<" "<<num - l;
+ 
+    
+return 0;
+}
