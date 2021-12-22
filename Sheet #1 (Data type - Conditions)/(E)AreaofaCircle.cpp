95902948e3e08bfa50e858ec927f8a35a4b3d3a2
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-22 21:23:39 +0530

    Create (E)AreaofaCircle.cpp

---
+++ Sheet #1 (Data type - Conditions)/(E)AreaofaCircle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+ 
+  long double r,pi=3.141592653;
+  cin>>r;
+  cout<< fixed <<setprecision(9)<<(pi*r*r);
+ return 0; 
+}
+ 
