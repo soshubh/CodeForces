a18ec04762fdb7607cac4e25ee79b05bfe583fd6
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-22 21:46:10 +0530

    Create (Z)HardCompare.cpp

---
+++ Sheet #1 (Data type - Conditions)/(Z)HardCompare.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if(b*log(a)>d*log(c))
+     cout<<"YES";
+    else
+     cout<<"NO"; 
+return 0;
+}
