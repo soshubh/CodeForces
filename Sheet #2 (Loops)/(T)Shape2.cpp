1d1bd3ce2b92980816032a5725e17c8a73f34fb3
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-26 19:05:53 +0530

    Create (T)Shape2.cpp

---
+++ Sheet #2 (Loops)/(T)Shape2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ int n;
+ cin>>n;
+  for(int i=1;i<=n;i++){
+   for(int j=n;j>i;j--){
+   cout<<" ";}
+
+   for(int k=1;k<=(2*i-1);k++)
+   {
+     cout<<"*";
+   }
+   cout<<endl;}
+  return 0;
+}
