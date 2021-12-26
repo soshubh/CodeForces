3678572e406e6e4735b782ebcdb3c7e8730a1eb6
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-26 18:35:03 +0530

    Create (Q)Digits.cpp

---
+++ Sheet #2 (Loops)/(Q)Digits.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while (t--)
+{
+  int n;
+  cin>>n;
+  while (n!=0)
+  {
+    cout<<n%10<<" ";
+    n=n/10;
+    }
+  cout<<endl;  
+  
+}
+
+  return 0;
+}
