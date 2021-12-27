4ee5704b49bce5e973d5d0c5d30cbf50954e5f88
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-27 21:25:09 +0530

    Create (W)Shape3.cpp

---
+++ Sheet #2 (Loops)/(W)Shape3.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ int n;
+ cin>>n;
+  for(int i=1;i<=n;i++){
+   for(int j=n;j>i;j--){
+   cout<<" ";}
+ 
+   for(int k=1;k<=(2*i-1);k++)
+   {
+     cout<<"*";
+   }
+   cout<<endl;}
+    for(int i=n;i>=1;i--){
+      for(int j=n;j>i;j--){
+         cout<<" ";}
+ 
+         for(int k=1;k<=(2*i-1);k++)
+          {
+        cout<<"*";
+         }
+   cout<<endl;}
+  return 0;
+}
