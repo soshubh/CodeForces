7bae0ba684c09244d24ace110030643d22677ebc
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-28 17:15:29 +0530

    Create (E)IntervalSweep.cpp

---
+++ Contest #1/(E)IntervalSweep.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a,b;
+  cin>>a>>b;
+  if((a-b)>=-1&&(a-b)<=1&&a!=0&&b!=0)
+   cout<<"YES";
+  else
+   cout<<"NO"; 
+return 0;
+}
