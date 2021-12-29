06843b1dedd9583efba1c168186e66a47f3ad6c7
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-29 17:32:51 +0530

    Create (D)Positionsinarray.cpp

---
+++ Sheet #3 (Arrays)/(D)Positionsinarray.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++)
+  {
+    cin>>arr[i];
+  }
+  for(int i=0;i<n;i++)
+  {
+    if(arr[i]<11)
+    cout<<"A["<<i<<"]"<<" = "<<arr[i]<<endl;
+  }
+         return 0;
+}
