552348a053828ed96e8922cdcbd1d2c9ac6c9848
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2021-12-29 17:32:10 +0530

    Create (C)Replacement.cpp

---
+++ Sheet #3 (Arrays)/(C)Replacement.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++)
+  {
+    cin>>arr[i];
+  }
+  for(int i=0;i<n;i++)
+  {
+    if(arr[i]>0)
+    cout<<"1"<<" ";
+    else if(arr[i]<0)
+    cout<<"2"<<" ";
+    else
+    cout<<arr[i]<<" ";
+  }
+         return 0;
+}
