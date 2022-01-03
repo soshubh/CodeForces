0bb6a34ec9c1adaa13cad6a776dd12a7617f7c1c
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2022-01-03 09:53:06 +0530

    Create (A)TimonandPumbaa.cpp

---
+++ Contest #2/(A)TimonandPumbaa.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int a,b;
+   cin>>a>>b;
+   long long int r=a-b;
+   if(r>=0)
+    cout<<r;
+   else
+     cout<<"0";
+         return 0;
+}
