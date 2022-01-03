a6f2851304a356427f07df805046e75c41989302
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2022-01-03 09:49:26 +0530

    Create (A)Squareorrectangle.cpp

---
+++ Contest #3.1/(A)Squareorrectangle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define ll long long int
+int main(){
+  int t;
+  cin>>t;
+  while (t--)
+  {
+   int w,h;
+    cin>>w>>h;
+    if(w==h){
+     cout<<"Square"<<endl;}
+    else{
+     cout<<"Rectangle"<<endl;}
+  }  
+}
