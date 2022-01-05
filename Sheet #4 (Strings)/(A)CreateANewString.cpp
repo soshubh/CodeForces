e75faa21a9a0b3627c4a8e4fa9a3d8ff5ea1d594
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2022-01-05 10:50:49 +0530

    Create (A)CreateANewString.cpp

---
+++ Sheet #4 (Strings)/(A)CreateANewString.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int m=1000;
+int main(){
+  char s[m],t[m];
+  cin>>s>>t;
+  int l=strlen(s);
+  cout<<l;
+  l=strlen(t);
+  cout<<" "<<l<<endl;
+  cout<<s<<" "<<t;
+  return 0;
+}  
