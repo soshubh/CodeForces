5808c725cd283908feec01ec5ffea001e9143ce6
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2022-01-07 10:59:22 +0530

    Create (B)Print.cpp

---
+++ Sheet #5 (Functions)/(B)Print.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+ll num(ll  a)
+{   
+    for(int i=1;i<=a;i++)
+     {cout<<i;
+      if(i!=a)
+       cout<<" ";
+      }
+}
+int main()
+{ ll a;
+   cin>>a;
+   num(a);
+}
