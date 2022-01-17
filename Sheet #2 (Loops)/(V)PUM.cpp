d20f7290b7e1de893eb92785396cfd9e217639e1
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2022-01-17 10:10:23 +0530

    Create (V)PUM.cpp

---
+++ Sheet #2 (Loops)/(V)PUM.cpp
@@ -0,0 +1,17 @@
+// soshubh
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int l;
+typedef long double lb;    
+int main()
+{
+    int n,p=1;
+    cin>>n;
+    while (n--)
+    {
+          for(int j=p;j<p+3;j++)
+           cout<<j<<" ";
+         cout<<"PUM"<<endl;  
+       p=p+4;
+    }
+}     
